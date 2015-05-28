791dbf713b87d5023d7b717826a563d3bdca61ce
baob <coder@onesandthrees.com>
2015-05-28 18:13:25 +0100

    two specs

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -8,8 +8,14 @@ describe("Tags", function () {
             var args = ["--depth=4", "--hello=world"],
                 results = tags.parse(args);
 
-            expect(results).to.have.a.property("depth", 4);
             expect(results).to.have.a.property("hello", "world");
+        });
+
+        it("should parse long formed tags and parse integers", function () {
+            var args = ["--depth=4", "--hello=world"],
+                results = tags.parse(args);
+
+            expect(results).to.have.a.property("depth", 4);
         });
     });
 
