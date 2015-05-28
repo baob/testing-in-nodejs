a16ffca673dfa4bf16189864af396fb9f641a9fe
baob <coder@onesandthrees.com>
2015-05-28 19:03:52 +0100

    keep jslint happy

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -19,24 +19,23 @@ describe("Tags", function () {
             expect(results).to.have.a.property("depth", 4);
         });
 
-        it("should fallback to defaults", function(){
-            var args = ["--depth=4", "--hello=world"];
-            var defaults = { depth: 2, foo: "bar" };
-            var results = tags.parse(args, defaults);
-
-            var expected = {
-                depth: 4,
-                foo: "bar",
-                hello: "world"
-            };
+        it("should fallback to defaults", function () {
+            var args = ["--depth=4", "--hello=world"],
+                defaults = { depth: 2, foo: "bar" },
+                results = tags.parse(args, defaults),
+                expected = {
+                    depth: 4,
+                    foo: "bar",
+                    hello: "world"
+                };
 
             expect(results).to.deep.equal(expected);
         });
 
 
-        it("should accept tags without values as a bool", function(){
-            var args = ["--searchContents"];
-            var results = tags.parse(args);
+        it("should accept tags without values as a bool", function () {
+            var args = ["--searchContents"],
+                results = tags.parse(args);
 
             expect(results).to.have.a.property("searchContents", true);
         });
