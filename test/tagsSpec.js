97ce67338cc93bc5a6fb5b8b779442b4cbc7fa8d
baob <coder@onesandthrees.com>
2015-05-28 18:24:47 +0100

    next test

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -17,6 +17,19 @@ describe("Tags", function () {
 
             expect(results).to.have.a.property("depth", 4);
         });
+        it("should fallback to defaults", function(){
+            var args = ["--depth=4", "--hello=world"];
+            var defaults = { depth: 2, foo: "bar" };
+            var results = tags.parse(args, defaults);
+
+            var expected = {
+                depth: 4,
+                foo: "bar",
+                hello: "world"
+            };
+
+            expect(results).to.deep.equal(expected);
+        });
     });
 
 });
