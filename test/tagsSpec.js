7a147245bb25a2ccafe3d415802eaefebc983172
baob <coder@onesandthrees.com>
2015-05-28 17:27:49 +0100

    actually, this is the first test

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -1,6 +1,16 @@
 var expect = require("chai").expect;
 var tags = require("../lib/tags.js");
 
-describe("Tags", function(){
+describe("Tags", function () {
+
+    describe("#parse()", function () {
+        it("should parse long formed tags", function () {
+            var args = ["--depth=4", "--hello=world"],
+                results = tags.parse(args);
+
+            expect(results).to.have.a.property("depth", 4);
+            expect(results).to.have.a.property("hello", "world");
+        });
+    });
 
 });
