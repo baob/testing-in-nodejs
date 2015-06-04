c192307cbdbab5d8593d526fbaa47347c5a0947c
baob <coder@onesandthrees.com>
2015-06-04 21:33:04 +0100

    passing meaningful feature (in rspec)

--- spec/command/ack_js_spec.rb
+++ spec/command/ack_js_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
+require 'pry'
 
 describe 'ack.js', type: :aruba  do
+  let(:command) { 'ack.js' }
 
-  describe 'when run with -d1 -q=.js' do
-    it 'runs' do
-      run_simple "ack.js -d=1 -q=.js"
+  context 'with two .js files and a .txt file' do
+    let!(:thing) { write_file('thing.js', '') }
+    let!(:thang) { write_file('thang.js', '') }
+    let!(:thong) { write_file('thong.txt', '') }
+
+    describe 'when run with -d1 -q=.js' do
+      let(:command_params) { '-d1 -q=.js' }
+
+      it 'finds the js files' do
+        run_simple "#{command} #{command_params}"
+
+        assert_partial_output('thing.js', all_output)
+        assert_partial_output('thang.js', all_output)
+      end
+
+      it 'does not find the txt file' do
+        run_simple "#{command} #{command_params}"
+
+        assert_no_partial_output('thong.txt', all_output)
+      end
     end
   end
 
